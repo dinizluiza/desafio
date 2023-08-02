d44055ddb7e6a0fe51e42f968d01b70dd9fdda92
dinizluiza <ldmml@cin.ufpe.br>
2023-08-02 03:10:35 -0300

    ajustes finais

--- problema.sql
+++ problema.sql
@@ -3,9 +3,7 @@
 -- PROBLEMA 1 e 2
 
 CREATE DATABASE hospital_staging;
-
 USE hospital_staging;
-
 CREATE SCHEMA stg_hospital_a;
 CREATE SCHEMA stg_hospital_b;
 CREATE SCHEMA stg_hospital_c;
@@ -19,7 +17,6 @@ CREATE TABLE stg_hospital_a.paciente (
     nome_mae VARCHAR(100),
     dt_atualizacao TIMESTAMP
 );
-
 CREATE TABLE stg_hospital_b.paciente (
     id INT PRIMARY KEY IDENTITY(1,1),
     nome VARCHAR(100),
@@ -28,7 +25,6 @@ CREATE TABLE stg_hospital_b.paciente (
     nome_mae VARCHAR(100),
     dt_atualizacao TIMESTAMP
 );
-
 CREATE TABLE stg_hospital_c.paciente (
     id INT PRIMARY KEY IDENTITY(1,1),
     nome VARCHAR(100),
@@ -50,7 +46,6 @@ INSERT INTO
     stg_hospital_a.paciente (nome, dt_nascimento, cpf, nome_mae, dt_atualizacao)
 VALUES
     ('Paciente A3', '2000-12-30', 54321678901, 'Mãe A3', CURRENT_TIMESTAMP);
-
 INSERT INTO
     stg_hospital_b.paciente (nome, dt_nascimento, cpf, nome_mae, dt_atualizacao)
 VALUES
@@ -63,7 +58,6 @@ INSERT INTO
     stg_hospital_b.paciente (nome, dt_nascimento, cpf, nome_mae, dt_atualizacao)
 VALUES
     ('Paciente B3', '1979-11-25', 67890123456, 'Mãe B3', CURRENT_TIMESTAMP);
-
 INSERT INTO
     stg_hospital_c.paciente (nome, dt_nascimento, cpf, nome_mae, dt_atualizacao)
 VALUES
@@ -76,7 +70,6 @@ INSERT INTO
     stg_hospital_c.paciente (nome, dt_nascimento, cpf, nome_mae, dt_atualizacao)
 VALUES
     ('Paciente C3', '2005-02-14', 43210987654, 'Mãe C3', CURRENT_TIMESTAMP);
-
 CREATE SCHEMA hospital_staging.stg_prontuario;
 
 -- VERSÃO 1
@@ -122,7 +115,6 @@ SELECT nome, dt_nascimento, cpf, nome_mae
 FROM stg_prontuario.paciente
 GROUP BY nome, dt_nascimento, cpf, nome_mae
 HAVING COUNT(*) > 1;
-
 -- Saída: Paciente B1	01-JAN-90	12345678901	Mãe B1
 
 ----------------------------------------------------------
@@ -133,7 +125,6 @@ SELECT nome, dt_nascimento, cpf, nome_mae, MAX(dt_atualizacao)
 FROM stg_prontuario.paciente
 GROUP BY nome, dt_nascimento, cpf, nome_mae
 HAVING COUNT(*) > 1;
-
 -- Saída: Paciente B1	01-JAN-90	12345678901	Mãe B1	02-JAN-90
 
 ----------------------------------------------------------
@@ -144,87 +135,54 @@ HAVING COUNT(*) > 1;
 
 -- PROBLEMA 7 -- consultar diagrama
 
--- como diagnostico, tipo e data dependem somente do n_protocolo, causa uma
--- dependência parcial, ferindo a 2FN, assim, é necessário criar uma nova tabela "protocolo"
-
 CREATE TABLE stg_prontuario.atendimento_medico (
     id_paciente INT NOT NULL, -- herda pois entidade fraca
     n_protocolo INT NOT NULL, -- discriminadora
+    tipo CHAR(1),
+    data_atendimento DATE,
     codigo_procedimento VARCHAR(100),  -- chave estrangeira pelo relacionamento "realiza"
     PRIMARY KEY (id_paciente, n_protocolo),
     FOREIGN KEY (id_paciente) REFERENCES stg_prontuario.paciente(id),
     FOREIGN KEY (codigo_procedimento) REFERENCES stg_prontuario.procedimento_medico(codigo_procedimento)
 );
-
-CREATE TABLE stg_prontuario.protocolo (
-    id_paciente INT NOT NULL,
-    n_protocolo INT NOT NULL,
-    tipo CHAR(1),
-    data_atendimento DATE,
-    PRIMARY KEY (id_paciente, n_protocolo),
-    FOREIGN KEY (id_paciente, n_protocolo) REFERENCES stg_prontuario.atendimento_medico(id_paciente, n_protocolo),
-);
-
--- atributo multivalorado de protocolo
+-- atributo multivalorado de atendimento médico
 CREATE TABLE stg_prontuario.diagnostico (
     id_paciente INT NOT NULL,
     n_protocolo INT NOT NULL,
     diagnostico VARCHAR(100) NOT NULL,
     PRIMARY KEY (id_paciente, n_protocolo, diagnostico), 
-    FOREIGN KEY (id_paciente, n_protocolo) REFERENCES stg_prontuario.protocolo(id_paciente, n_protocolo)
+    FOREIGN KEY (id_paciente, n_protocolo) REFERENCES stg_prontuario.atendimento_medico(id_paciente, n_protocolo)
 );
 
 ----------------------------------------------------------
 
 -- PROBLEMA 8
 
-ALTER TABLE stg_prontuario.protocolo
+ALTER TABLE stg_prontuario.atendimento_medico
 ADD CONSTRAINT CK_tipo_atendimento
 CHECK (tipo IN ('I', 'U', 'A'));
 
 -- POVOAMENTO ATENDIMENTO simulação
 -- INSERT INTO stg_prontuario.atendimento_medico
---     (id_paciente, n_protocolo, codigo_procedimento)
+--     (id_paciente, n_protocolo, tipo, data_atendimento, codigo_procedimento)
 -- VALUES
---     (1, 1, '1010101');
+--     (1, 1, 'U', '2023-07-31', '1010101');
 -- INSERT INTO stg_prontuario.atendimento_medico
---     (id_paciente, n_protocolo, codigo_procedimento)
+--     (id_paciente, n_protocolo, tipo, data_atendimento, codigo_procedimento)
 -- VALUES
---     (2, 2, '2020202');
+--     (2, 2, 'U', '2023-08-01', '2020202');
 -- INSERT INTO stg_prontuario.atendimento_medico
---     (id_paciente, n_protocolo, codigo_procedimento)
+--     (id_paciente, n_protocolo, tipo, data_atendimento, codigo_procedimento)
 -- VALUES
---     (3, 3, '3030303 ');
+--     (3, 3, 'U', '2023-08-02', '3030303 ');
 -- INSERT INTO stg_prontuario.atendimento_medico
---     (id_paciente, n_protocolo, codigo_procedimento)
+--     (id_paciente, n_protocolo, tipo, data_atendimento, codigo_procedimento)
 -- VALUES
---     (4, 4, '2020202');
+--     (4, 4, 'I', '2023-08-02', '2020202');
 -- INSERT INTO stg_prontuario.atendimento_medico
---     (id_paciente, n_protocolo, codigo_procedimento)
+--     (id_paciente, n_protocolo, tipo, data_atendimento, codigo_procedimento)
 -- VALUES
---     (5, 5, '1010101');
-
--- POVOAMENTO PROTOCOLO simulação
--- INSERT INTO stg_prontuario.protocolo
---     (id_paciente, n_protocolo, tipo, data_atendimento)
--- VALUES
---     (1, 1, 'U', '2023-07-31');
--- INSERT INTO stg_prontuario.protocolo
---     (id_paciente, n_protocolo, tipo, data_atendimento)
--- VALUES
---     (2, 2, 'U', '2023-08-01');
--- INSERT INTO stg_prontuario.protocolo
---     (id_paciente, n_protocolo, tipo, data_atendimento)
--- VALUES
---     (3, 3, 'U', '2023-08-02');
--- INSERT INTO stg_prontuario.protocolo
---     (id_paciente, n_protocolo, tipo, data_atendimento)
--- VALUES
---     (4, 4, 'I', '2023-08-02');
--- INSERT INTO stg_prontuario.protocolo
---     (id_paciente, n_protocolo, tipo, data_atendimento)
--- VALUES
---     (5, 5, 'A', '2023-08-03');
+--     (5, 5, 'A', '2023-08-03', '1010101');
 
 -- POVOAMENTO DIAGNOSTICO simulação
 -- INSERT INTO stg_prontuario.diagnostico (id_paciente, n_protocolo, diagnostico)
@@ -249,11 +207,10 @@ CHECK (tipo IN ('I', 'U', 'A'));
 SELECT AVG(qtd_diagnosticos) AS media_diagnosticos_tipo_U
 FROM (
     SELECT COUNT(*) AS qtd_diagnosticos
-    FROM stg_prontuario.diagnostico d
-    INNER JOIN stg_prontuario.protocolo p ON p.n_protocolo = d.n_protocolo AND p.id_paciente = d.id_paciente
-    INNER JOIN stg_prontuario.atendimento_medico a ON p.n_protocolo = a.n_protocolo AND p.id_paciente = a.id_paciente
-    WHERE p.tipo = 'U'
-	GROUP BY p.id_paciente, p.n_protocolo
+    FROM atendimento_medico am
+    INNER JOIN diagnostico d ON am.id_paciente = d.id_paciente AND am.n_protocolo = d.n_protocolo
+    WHERE am.tipo = 'U'
+	GROUP BY am.id_paciente, am.n_protocolo
 );
 
 -- Saída: 2
